b3af04ee804bdef8c2e4936ee2090d50748333c0
Omar Shah <oshah1@terpmail.umd.edu>
2022-02-20 09:27:17 -0500

    Didn't modify a whole lot.

--- discussions/d1_intro_ruby/src/disc1.rb
+++ discussions/d1_intro_ruby/src/disc1.rb
@@ -3,31 +3,45 @@
 # The class Table represents a collection of tuples.
 
 class Tuple
+    @hash = Hash.new{0}
 
     # data is an array of values for the tuple
     def initialize(data)
-        raise "unimplemented"
+	@data = data
+	@@hash[data.length] += 1
+        
     end
 
     # This method returns the data at a particular index of a tuple (0 indexing)
     # If the provided index exceeds the largest index in the tuple, nil should be returned.
     # index is an Integer representing a valid index in the tuple.
     def getData(index)
-        raise "unimplemented"
+	if index >= @data.length then
+	    return nil
+	else
+	return @data[index]
+        end
     end
 
+    def getSize()
+	return @data.length
+    end
     # This method should return the number of tuples of size n that have ever been created
     # hint: you should use a static variable
     # hint2: a hash can be helpful (though not strictly necessary!)
-    def self.getNumTuples(n) 
-        raise "unimplemented"
+    def self.getNumTuples(n)
+	 return @hash[n]
+        
     end
 end
 
 class Table
+    
     # column_names is an Array of Strings
     def initialize(column_names)
-        raise "unimplemented"
+	@column_names = column_names
+	@tuples = []
+        
     end
 
     # This method inserts a tuple into the table.
@@ -37,13 +51,29 @@ class Table
     # otherwise, DO NOT insert the tuple and return false instead.
     # tuple is an instance of class Tuple declared above.
     def insertTuple(tuple)
-        raise "unimplemented"
+	if column_names.length == tuple.getSize then
+	    @tuples.push(tuple);
+	    return true
+	else
+            return false
+	end
     end
     
     # Given a column name and a value, this method finds the number of rows where the value 
     # for the column matches the given value.
     def numRowsWhere(column,value)
-        raise "unimplemented" 
+	count = 0
+
+	column_index = @column_names.index(column);
+	for tuple in @tuples do
+	    
+	    if tuple.getData(column_index) == value then
+		count += 1
+	    end
+
+	end
+
+	return count
     end 
 
 end
--- discussions/d3_intro_ocaml/src/disc3.ml
+++ discussions/d3_intro_ocaml/src/disc3.ml
@@ -1,37 +1,74 @@
-# Introduction
 
 
-let positive n = raise Exception, "Not implemented"
 
-let double x = raise Exception, "Not implemented"
-
-let fizz n = raise Exception, "Not implemented"
+let positive n = if n > 0 then "positive" 
+  else if n < 0 then "negative" else "zero"
 
 
-# Problems 
+let double x = 2 * x;;
+
+let fizz n = 
+  if n mod 15 =0 then "fizz buzz"
+  else if n mod 3 =0 then "fizz"
+  else if n mod 5 =0 then "buzz"
+  else ""
 
 
-let first_elem lst = raise Exception, "Not implemented"
-
-let rec sum lst = raise Exception, "Not implemented"
-
-let max_list lst = raise Exception, "Not implemented"
-
-let sum_tuple t = raise Exception, "Not implemented"
 
 
-# Multi-Function Problems
 
 
-let rec product lst = raise Exception, "Not implemented"
 
-let rec max_product lst = raise Exception, "Not implemented"
+let first_elem lst = 
+  match lst with
+| h::t -> h
+| [] -> 0
 
-let rec length l = raise Exception, "Not implemented" 
+let rec sum lst = 
+  match lst with
+  | [] -> 0
+  | h::t -> h + (sum t)
 
-let rec check_matrix_aux lst len = raise Exception, "Not implemented"
+let rec max_list lst = 
+match lst with
+| [] -> 0
+| [x] -> x
+| h::t -> max h (max_list t)
 
-let check_matrix lst = raise Exception, "Not implemented"
+(*max elme1 elem2*)
+let sum_tuple t = 
+  match t with
+  | (a, b, c) -> a + b + c
+
+
+
+
+(*we have a function for the max number in a list*)
+let rec product lst = 
+  match lst with
+  | [] -> 1
+  | h::t -> h * (product t)
+
+let rec max_product lst = 
+  match lst with
+  | [] -> 0
+  | h::t -> max (product h) (max_product t)
+
+let rec length l = 
+  match l with
+  | [] -> 0
+  | h::t -> 1 + (length t)
+
+let rec check_matrix_aux lst len = 
+  match lst with
+  | [] -> true
+  | h::t -> if (length h) = len then check_matrix_aux t len
+             else false
+
+let check_matrix lst = 
+  match lst with
+  | [] -> true
+  | h::t -> check_matrix_aux lst (length h) (*compare other lists with length of head*)
 
 
 
